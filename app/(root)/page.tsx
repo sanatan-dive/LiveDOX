2374e271bdbfa62ef5659ccdab212484cb4fb667
sanatan-dive <sanatansharma352@gmail.com>
2024-11-06 01:30:13 +0530

    auth clerk done liveblocks room done

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+   <div>
+    <h1>Home</h1>
+    <Button variant="default">Button</Button>
+   </div>
+  );
+}
